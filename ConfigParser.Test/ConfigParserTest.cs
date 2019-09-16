e002c9197b8d1f72aaff81560e5bd2c06777aed8
s-yk <sonic_cave@hotmail.co.jp>
2019-09-16 23:46:10 +0900

    テストクラス作ったり

---
+++ ConfigParser.Test/ConfigParserTest.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using NUnit.Framework;
+
+namespace ConfigParser
+{
+    public class ConfigParserTest
+    {
+        private Parser parser;
+
+        [SetUp]
+        public void Setup()
+        {
+            parser = new Parser();
+        }
+
+        [Test]
+        public void Parseメソッドを使用してファイルを読み込むことができる()
+        {
+            parser.Parse("resources/test1.txt");
+        }
+
+        [Test]
+        public void ファイルが存在しない場合はFileNotFoundExceptionをスローする()
+        {
+            Assert.Throws<FileNotFoundException>(() => parser.Parse("resources/test1.txt_"));
+        }
+    }
+}
---
+++ ConfigParser/Parser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConfigParser
+{
+    public class Parser
+    {
+        public void Parse(string path)
+        {
+            using (var fs = File.OpenRead(path))
+            using (var sr = new StreamReader(fs, Encoding.UTF8))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
